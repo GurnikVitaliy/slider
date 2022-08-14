817b3327ad9560271a52fefc9c2434738d1f02f0
GurnikVitaliy <111244081+GurnikVitaliy@users.noreply.github.com>
2022-08-15 00:24:35 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const btnPrev = document.getElementById('prev'),
+      btnNext = document.getElementById('next'),
+      sliders = document.querySelectorAll('.slider');
+
+let index = 0;
+
+function activeSlider(ind) {
+    console.log(ind);
+    for(let slider of sliders) {
+        slider.classList.remove('active');
+    }
+    sliders[ind].classList.add('active');
+}
+
+function prevSlider() {
+    if(index === 0) {
+        index = sliders.length - 1;
+        activeSlider(index);
+    }
+    else {
+        index--;
+        activeSlider(index);
+    }
+}
+
+function nextSlider() {
+    if(index === sliders.length - 1) {
+        index = 0;
+        activeSlider(index);
+    }
+    else {
+        index++;
+        activeSlider(index);
+    }
+}
+
+btnPrev.addEventListener('click', nextSlider);
+btnNext.addEventListener('click', prevSlider);
